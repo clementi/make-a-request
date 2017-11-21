be0fb4780ecb4792facf594ebbaff2ec2244e001
Jeffrey Pratt <clementi@users.noreply.github.com>
2017-11-20 21:58:54 -0700

    Remove main doc string

--- clojure-http/src/clojure_http/core.clj
+++ clojure-http/src/clojure_http/core.clj
@@ -3,7 +3,6 @@
   (:require [clj-http.client :as client]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println (:body (client/get "https://httpbin.org/anything")
                   {:accept :json})))
