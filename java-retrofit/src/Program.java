653cfaae705f8041f196bf9079fb01a910864860
Jeffrey Pratt <clementi@users.noreply.github.com>
2017-12-06 16:59:44 -0700

    Add Retrofit example

---
+++ java-retrofit/src/Program.java
@@ -0,0 +1,31 @@
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
+import java.io.IOException;
+
+public class Program {
+    public static void main(String[] args) {
+        Retrofit retrofit = new     Retrofit.Builder()
+                .baseUrl("https://httpbin.org")
+                .build();
+
+        HttpBin httpBin = retrofit.create(HttpBin.class);
+
+        Call<ResponseBody> call = httpBin.anything();
+
+        try {
+            System.out.println(call.execute().body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private interface HttpBin {
+        @GET("/anything")
+        @Headers("Accept: application/json")
+        Call<ResponseBody> anything();
+    }
+}
