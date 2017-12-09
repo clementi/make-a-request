3b412b8f49a96fca70cf2bca25da4feb4f7aced0
Jeffrey Pratt <clementi@users.noreply.github.com>
2017-12-08 20:04:05 -0700

    Eliminate  method

--- elixir/lib/elixirhttp.ex
+++ elixir/lib/elixirhttp.ex
@@ -1,8 +1,4 @@
 defmodule Elixirhttp do
-  def hello do
-    :world
-  end
-
   def start(_type, _args) do
     response = HTTPotion.get "httpbin.org/anything", headers: ["Accept": "application/json"]
     IO.puts response.body
