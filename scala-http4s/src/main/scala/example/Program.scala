b4922b11b23f1683dbd301a6e4a4fb7737a66997
Jeffrey Pratt <clementi@users.noreply.github.com>
2020-12-23 16:33:11 -0700

    Create request with Accept header

--- scala-http4s/src/main/scala/example/Program.scala
+++ scala-http4s/src/main/scala/example/Program.scala
@@ -1,7 +1,11 @@
 package example
 
 import cats.effect.{ContextShift, IO, Timer}
-import org.http4s.client.blaze._
+import org.http4s.MediaType
+import org.http4s.Method.GET
+import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.client.dsl.io._
+import org.http4s.headers.Accept
 import org.http4s.implicits._
 
 import scala.concurrent.ExecutionContext.global
@@ -11,6 +15,12 @@ object Program extends App {
   implicit val timer: Timer[IO] = IO.timer(global)
 
   BlazeClientBuilder[IO](global).resource
-    .use(_.expect[String](uri"http://httpbin.org/anything").map(println))
+    .use { client =>
+      val request = GET(
+        uri"http://httpbin.org/anything",
+        Accept(MediaType.application.json)
+      )
+      client.expect[String](request).map(println)
+    }
     .unsafeRunSync()
 }
--- scala-http4s/src/main/scala/example/ProgramAsIOApp.scala
+++ scala-http4s/src/main/scala/example/ProgramAsIOApp.scala
@@ -1,7 +1,11 @@
 package example
 
 import cats.effect.{ExitCode, IO, IOApp}
+import org.http4s.MediaType
+import org.http4s.Method.GET
 import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.client.dsl.io._
+import org.http4s.headers.Accept
 import org.http4s.implicits._
 
 import scala.concurrent.ExecutionContext.global
@@ -9,7 +13,13 @@ import scala.concurrent.ExecutionContext.global
 object ProgramAsIOApp extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
     BlazeClientBuilder[IO](global).resource
-      .use(_.expect[String](uri"http://httpbin.org/anything").map(println))
+      .use { client =>
+        val request = GET(
+          uri"http://httpbin.org/anything",
+          Accept(MediaType.application.json)
+        )
+        client.expect[String](request).map(println)
+      }
       .as(ExitCode.Success)
   }
 }
