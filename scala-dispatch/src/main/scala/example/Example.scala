043e47840b883d355a402b44ca7ff393e54af5f0
Jeffrey Pratt <clementi@users.noreply.github.com>
2020-12-25 15:39:44 -0700

    Cleanup

--- scala-dispatch/src/main/scala/example/Example.scala
+++ scala-dispatch/src/main/scala/example/Example.scala
@@ -1,13 +1,12 @@
 package example
 
-import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 import dispatch._
 
 object Example extends App {
-  val service = url("https://httpbin.org/anything")
+  val service = host("httpbin.org") / "anything"
   val response = Http.default(service OK as.String)
 
   response.map(println)
