489b17b2c7cfb0fe6e6494b7130def79c8f9a6e5
Jeff Pratt <jpratt@proofpoint.com>
2022-03-15 16:25:53 -0600

    Add csharp-flurl example

---
+++ csharp-flurl/Program.cs
@@ -0,0 +1,9 @@
+﻿using Flurl;
+using Flurl.Http;
+
+var result = await "https://httpbin.org"
+    .AppendPathSegment("anything")
+    .SetQueryParams(new { name = "USS Enterprise", registry = "NCC-1701" })
+    .GetStringAsync();
+
+Console.WriteLine(result);
